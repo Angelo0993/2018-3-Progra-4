a9e73c9292806904f2081ed41462ff80fd655d37
Laboratorios <Laboratorios@E21>
2018-11-17 10:45:00 -0600

    proyecto de .Model para acceder a BD

---
+++ PrograIV.AW.Products/PrograIV.AW.Products.Model/MisTablas/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograIV.AW.Products.Model
+{
+    public partial class Product
+    {
+        public string SizeInInches { get {
+                string elResultado = Size;
+                if (SizeUnitMeasureCode != null && SizeUnitMeasureCode == "CM")
+                {
+                    float elTamanoFloat = 0;
+                    if (float.TryParse(Size, out elTamanoFloat))
+                        elResultado = (elTamanoFloat / (float)2.54).ToString();
+                }
+                return elResultado;
+            }
+            set { }
+        }
+    }
+}
